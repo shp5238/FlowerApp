47f8164c7d793da92370476e7c6a7aac4e89cdf2
Shreya <shreyapasupuleti@gmail.com>
2025-06-10 15:38:27 -0700

    Initial Commit

---
+++ FlowerApp/FlowerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowerAppApp.swift
+//  FlowerApp
+//
+//  Created by Shreya Pasupuleti on 6/10/25.
+//
+
+import SwiftUI
+
+@main
+struct FlowerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlowerAppTests/FlowerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlowerAppTests.swift
+//  FlowerAppTests
+//
+//  Created by Shreya Pasupuleti on 6/10/25.
+//
+
+import Testing
+@testable import FlowerApp
+
+struct FlowerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
